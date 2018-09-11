390b7c3bca5dcfdd70f5d7915f9acf1fd27f89f0
grace <glee8804@gmail.com>
2018-09-11 22:02:33 +0900

    shorten unnecessary code lines add test.html for testing modify port 5000

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-'use strict';
-
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const {app, BrowserWindow} = require('electron');
 const path = require('path');
 
 // Keep a global reference of the mainWindowdow object, if you don't, the mainWindowdow will
@@ -10,16 +6,8 @@ const path = require('path');
 var mainWindow = null;
 var subpy = null;
 
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser mainWindow.
-// Some APIs can only be used after this event occurs.
-app.on('ready', function() {
-	
-	// spawn server
-	subpy = require('child_process').spawn('python', [__dirname + '/web_app/run_app.py']);
-
-  // Create the browser mainWindow
+function createWindow(){
+	// Create the browser mainWindow
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -33,32 +21,44 @@ app.on('ready', function() {
   });
 
     // Load the index page
-    mainWindow.loadURL('http://localhost:4040/');
-  
+	mainWindow.loadFile('./test.html')
+  // mainWindow.loadURL('http://localhost:5000/');
+
 	// Open the DevTools.
-	//mainWindow.webContents.openDevTools();
+	mainWindow.webContents.openDevTools();
 
 	// Emitted when the mainWindow is closed.
-	mainWindow.on('closed', function() {
+	mainWindow.on('closed', ()=>{
     // Dereference the mainWindow object
     mainWindow = null;
   });
-	
+}
+// This method will be called when Electron has finished
+// initialization and is ready to create browser mainWindow.
+// Some APIs can only be used after this event occurs.
+app.on('ready', ()=>{
+
+	// spawn server
+	subpy = require('child_process').spawn('python3', [__dirname + '/web_app/run_app.py']);
+  createWindow()
+
 });
 
 // disable menu
-electron.app.on('browser-window-created',function(e,window) {
+app.on('browser-window-created', (e,window)=>{
     window.setMenu(null);
 });
 
 // ------- app terminated
-
-app.on('window-all-closed', function() {
-	// quit app if windows are closed
-  app.quit();
+app.on('window-all-closed', ()=>{
+	// On macOs it is common for application and their icon
+  // to stay active until the user quits explicitly with Cmd + q
+  if(process.platform !== 'darwin'){
+    app.quit()
+  }
 });
 
-app.on('quit', function() {
+app.on('quit', ()=>{
 	// kill the python server on exit
   subpy.kill('SIGINT');
 });
--- web_app/run_app.py
+++ web_app/run_app.py
@@ -4,11 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return '<a href="/user/Kyle">Click</a>'
+	# return '<a href="/user/Kyle">Click</a>'
+	return '<h1>Hello World</h1>'
 
 @app.route('/user/<name>')
 def user(name):
-	return '<h1>Hello, %s!</h1>' % name    
+	return '<h1>Hello, %s!</h1>' % name
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=4040, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
