5e45df4ef5a4c2651c42d40109618e8c14a85290
grace <glee8804@gmail.com>
2018-09-12 11:25:07 +0900

    add python function comment format add sample piechart from D3

--- py_source/run_app.py
+++ py_source/run_app.py
@@ -4,10 +4,21 @@ app = Flask(__name__, template_folder='../web/', static_folder='../web/resource'
 
 @app.route('/')
 def index():
+	"""
+	Rerount to index html
+	"""
 	return render_template('index.html')
 
 @app.route('/user/<name>', methods=['GET', 'POST'])
 def user(name):
+	"""
+	Move to user.html with given user name
+
+	Args:
+		name (str) : user name
+	Returns:
+		html : user.html
+	"""
 	return render_template('user.html', name=name)
 
 if __name__ == '__main__':
