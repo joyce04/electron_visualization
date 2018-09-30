bd198eb2955bd8941eb131e2b4a3ec9dfcef65a0
Kyle You <madigun697@gmail.com>
2018-09-30 21:58:41 +0900

    Upgrade Grid
    1. Add Table Title
    2. Determine column width by column contents
    3. Resize Grid
    4. Change locale (KR -> EN) for search menu
    5. Add search button and detail view button

--- py_source/run_app.py
+++ py_source/run_app.py
@@ -43,11 +43,12 @@ def open_chart(chart_type):
 
 @app.route('/grid', methods=['GET', 'POST'])
 def open_grid():
-	return render_template('grid.html', fext='none', column = [], data = '{}')
+	return render_template('grid.html', fname = '', fext='none', column = [], colw = [], data = '{}')
 
 @app.route('/upload_file', methods=['POST'])
 def upload_file():
 	f = request.files['file']
+	fname = f.filename.split('.')[0]
 	fext = f.filename.split('.')[1]
 	if fext == 'csv':
 		df = pd.read_csv(f, index_col=0).reset_index(drop=True)
@@ -56,11 +57,18 @@ def upload_file():
 	else:
 		df = pd.DataFrame(['Not Available'], columns=['Col'])
 
+	col_width = [(df[c].apply(lambda x: len(str(x))).values + [len(c)]).max() for c in df.columns]
+	col_width = [cw * 7 + 10 for cw in col_width]
+	if sum(col_width) > 1278:
+		col_width = [0 if cw > 1278 / len(col_width) else cw for cw in col_width]
+			
+	col_width = [(1278 - sum(col_width)) / len([cw for cw in col_width if cw == 0]) if cw == 0 else cw for cw in col_width]
+
 	json_data = collections.OrderedDict()
 	json_data['rows'] = df.to_dict(orient='records')
 	jsonp = json.loads(json.dumps(json_data, ensure_ascii=False, indent='\t').replace('`', ''))
 
-	return render_template('grid.html', fext = fext, column = df.columns.tolist(), data = jsonp)
+	return render_template('grid.html', fname = fname, fext = fext, column = df.columns.tolist(), colw = col_width, data = jsonp)
 
 
 if __name__ == '__main__':
