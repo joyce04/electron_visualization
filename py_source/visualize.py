f824da89099ce80e558f51de0f64d8f27e878d2d
joyce04 <glee8804@gmail.com>
2018-12-20 23:42:38 +0900

    modify order

--- py_source/visualize.py
+++ py_source/visualize.py
@@ -52,6 +52,9 @@ def get_visualization_json(cluster_K, documents, tsne_result, lda_vis_data, lda_
             for idx, row in other_lda_df[other_lda_df.doc_ratio_rank == i].sort_values('doc_ratio', ascending=False).iterrows():
                 if int(row.topic_lda) not in lda_value_topic_map.values() and int(row[col_name]) not in lda_value_topic_map.keys():
                     lda_value_topic_map[int(row[col_name])] = int(row.topic_lda)
+            if cluster_K - len(lda_value_topic_map) == 1:
+                
+
             if len(lda_value_topic_map) == cluster_K:
                 break;
 
@@ -60,44 +63,33 @@ def get_visualization_json(cluster_K, documents, tsne_result, lda_vis_data, lda_
 
         return lda_key_topic_map, lda_value_topic_map
 
-    def get_hbar_chart_json(vis_data, method):
+    def get_hbar_chart_json(vis_data, method, mapper):
         hbar_json = {}
         hbar_json['labels'] = vis_data.topic_info.Category.unique().tolist()
         hbar_json['max_width'] = vis_data.topic_info[vis_data.topic_info.Category != 'Default'][['Total']].max()[0] * 1.
         for l in vis_data.topic_info.Category.unique().tolist():
+            real_topic = l if mapper == None or l == 'Default' else 'Topic%d' % mapper[int(l.replace('Topic', ''))]
             tmp_df = vis_data.topic_info[vis_data.topic_info.Category == l].sort_values(['Category', 'Freq'], ascending=[True, False])
             tmp_df.Total = tmp_df.Total.apply(lambda x: x * 1.)
-            hbar_json[l] = list(tmp_df[['Term', 'Freq', 'Total']].sort_values('Freq', ascending=False).reset_index().to_dict('index').values())
+            hbar_json[real_topic] = list(tmp_df[['Term', 'Freq', 'Total']].sort_values('Freq', ascending=False).reset_index().to_dict('index').values())
 
         write_json_to_file('./Visualization/res/%s/hbar_data.json' % method, hbar_json)
         return json.loads(json.dumps(hbar_json, ensure_ascii=False, indent='\t').replace('`', ''))
 
-    def get_scatter_chart_json(doc_result, topic_array, method):
-        doc_result['topic'] = topic_array
+    def get_scatter_chart_json(doc_result, method):
         doc_result = pd.merge(doc_result, pd.DataFrame(tsne_result, columns=['plot_x', 'plot_y']), left_index=True, right_index=True)
 
-        scatter_json = list(doc_result[['id', 'plot_x', 'plot_y', 'topic']].to_dict('index').values())
+        scatter_json = list(doc_result[['id', 'plot_x', 'plot_y', 'topic_%s' % method]].to_dict('index').values())
 
         write_json_to_file('./Visualization/res/%s/scatter_data.json' % method, scatter_json)
 
-        if 'data_output' not in os.listdir():
-            os.mkdir('data_output')
-
-        doc_result.to_csv(os.path.join(os.getcwd(), 'data_output', '%s.tsv' % method), sep='\t', index_label=False)
-
         return json.loads(json.dumps(scatter_json, ensure_ascii=False, indent='\t').replace('`', ''))
 
-    def get_merged_table_json():
-        lda_result = pd.read_csv('./data_output/lda.tsv', sep='\t')
-        km_result = pd.read_csv('./data_output/km.tsv', sep='\t')
-        dec_result = pd.read_csv('./data_output/dec.tsv', sep='\t')
-
-        lda_docs_by_topic = lda_result.groupby('topic').agg({'id': 'unique'})
-        km_docs_by_topic = km_result.groupby('topic').agg({'id': 'unique'})
-        dec_docs_by_topic = dec_result.groupby('topic').agg({'id': 'unique'})
-
-        merged_result = pd.merge(lda_result[['id', 'document', 'topic']], km_result[['id', 'topic']], on='id', suffixes=('_lda', '_km'))
-        merged_result = pd.merge(merged_result[['id', 'document', 'topic_lda', 'topic_km']], dec_result[['id', 'topic']], on='id')
+    def get_merged_table_json(document, lda_labels, km_labels, dec_labels):
+        merged_result = document[['id', 'document']]
+        merged_result['topic_lda'] = lda_labels
+        merged_result['topic_km'] = km_labels
+        merged_result['topic_dec'] = dec_labels
         merged_result.columns = ['id', 'document', 'topic_lda', 'topic_km', 'topic_dec']
 
         json_data = collections.OrderedDict()
@@ -106,23 +98,26 @@ def get_visualization_json(cluster_K, documents, tsne_result, lda_vis_data, lda_
         lda_km_topic_map, km_lda_topic_map = get_topic_map(pd.DataFrame(json_data['rows']), 'topic_km')
         lda_dec_topic_map, dec_lda_topic_map = get_topic_map(pd.DataFrame(json_data['rows']), 'topic_dec')
 
+        merged_result['topic_km'] = merged_result.topic_km.apply(lambda x: km_lda_topic_map[x])
+        merged_result['topic_dec'] = merged_result.topic_dec.apply(lambda x: dec_lda_topic_map[x])
+
         json_data['lda_km_topic_map'] = lda_km_topic_map
         json_data['km_lda_topic_map'] = km_lda_topic_map
         json_data['lda_dec_topic_map'] = lda_dec_topic_map
         json_data['dec_lda_topic_map'] = dec_lda_topic_map
 
         write_json_to_file('./Visualization/res/document_table.json', json_data)
-        return  json.loads(json.dumps(json_data, ensure_ascii=False, indent='\t').replace('`', ''))
+        return  json.loads(json.dumps(json_data, ensure_ascii=False, indent='\t').replace('`', '')), merged_result, km_lda_topic_map, dec_lda_topic_map
+
+    document_table_json, mapped_documents, km_lda_topic_map, dec_lda_topic_map = get_merged_table_json(documents, lda_labels, km_labels, dec_labels)
 
     lda_hbar_json = get_hbar_chart_json(lda_vis_data, 'lda')
-    lda_scatter_json = get_scatter_chart_json(documents, lda_labels, 'lda')
+    lda_scatter_json = get_scatter_chart_json(mapped_documents, 'lda', None)
 
     km_hbar_json = get_hbar_chart_json(km_vis_data, 'km')
-    km_scatter_json = get_scatter_chart_json(documents, km_labels, 'km')
+    km_scatter_json = get_scatter_chart_json(mapped_documents, 'km', km_lda_topic_map)
 
     dec_hbar_json = get_hbar_chart_json(dec_vis_data, 'dec')
-    dec_scatter_json = get_scatter_chart_json(documents, dec_labels, 'dec')
-
-    document_table_json = get_merged_table_json()
+    dec_scatter_json = get_scatter_chart_json(mapped_documents, 'dec', dec_lda_topic_map)
 
     return lda_hbar_json, km_hbar_json, dec_hbar_json, lda_scatter_json, km_scatter_json, dec_scatter_json, document_table_json
